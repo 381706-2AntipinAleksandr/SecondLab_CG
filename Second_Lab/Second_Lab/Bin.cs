55a898cbc6954822d230eb162bd108afd46bb5d9
381706-2AntipinAleksandr <antipin.aleksandr.1999@mail.ru>
2019-04-26 09:40:03 +0300

    first class

---
+++ Second_Lab/Second_Lab/Bin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Second_Lab
+{
+    class Bin
+    {
+        public static int LenX, LenY, LenZ;
+        public static short[] array;
+
+        public Bin()
+        {
+        }
+
+        public void ReadBin(string name)
+        {
+            if (File.Exists(name))
+            {
+                BinaryReader reader = new BinaryReader(File.Open(name, FileMode.Open));
+                LenX = reader.ReadInt32();
+                LenY = reader.ReadInt32();
+                LenZ = reader.ReadInt32();
+
+                array = new short[LenX * LenY * LenZ];
+                for (int i = 0; i < LenX * LenY * LenZ; i++)
+                    array[i] = reader.ReadInt16();  
+            }
+        }
+    }
+}
---
+++ Second_Lab/Second_Lab/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL4;
+
+namespace Second_Lab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
